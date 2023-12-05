3199dd99bf6e8e76f996016df098df6956fb9fc0
Carlos Assunção <61700427+CarlosAssuncao@users.noreply.github.com>
2023-12-05 19:14:34 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Oi, tudo bem?</h1>
+  return <h1>Oi, tudo bem?</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste ");
 }
 
 export default Home;
