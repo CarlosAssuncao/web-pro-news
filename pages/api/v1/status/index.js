27566a49c78c45beac763453403ffb7e2bbbc7d4
Carlos Assunção <61700427+CarlosAssuncao@users.noreply.github.com>
2023-12-12 15:54:44 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  //response.status(200).send("são acima da média");
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "são acima da média" });
 }
 
